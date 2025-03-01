9a92fb4f91e6314786845c5c36873e6ae03a0c1d
zichen <974468854@qq.com>
2025-03-01 14:50:31 +0800

    finished hw0A

--- hw0A/src/JavaExercises.java
+++ hw0A/src/JavaExercises.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class startriangle {
+
+
+public class JavaExercises {
+
+    public static void starTriangle(){
+
+        for (int i = 0; i < 5; i ++){
+            String line = "";
+            for (int j = 0; j < i + 1; j ++ ){
+                line += "*";
+            }
+            System.out.println(line);
+        }
+    }
+
+    public static int quadrant(int x, int y){
+        if (x == 0 || y == 0){
+            return 0;
+        }else if (y > 0  && x > 0){
+            return 1;
+        }else if (y > 0  && x < 0){
+            return 2;
+        }else if (x < 0  && y < 0){
+            return 3;
+        }else {
+            return 4;
+        }
+    }
+
 }
--- hw0A/test/JavaExercisesTest.java
+++ hw0A/test/JavaExercisesTest.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class JavaExercisesTest {
+import org.junit.jupiter.api.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+public class JavaExercisesTest {
+    @Test
+
+    public void test() {
+        JavaExercises.starTriangle();
+
+    }
 }
